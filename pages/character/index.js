661bf8ca166ce04c9d8c61c159ae8b5e58e71828
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-10 01:11:17 -0500

    feat(character page): create a new 'CharacterPage' component

---
+++ pages/character/index.js
@@ -0,0 +1,37 @@
+export default function CharacterPage() {
+	return (
+		<div>
+			<h1>Luke Skywalker</h1>
+			<ul>
+				<div>
+					<h3>About Me:</h3>
+					<ul>
+						<li>Height: 5ft 10in</li>
+						<li>Mass: 160 lbs</li>
+						<li>Gender: Male</li>
+						<li>Eye Color: Brown</li>
+						<li>Hair Color: Brown</li>
+						<li>Birth Year: 1954</li>
+						<li>Species: Human</li>
+						<li>Homeworld: Earth</li>
+					</ul>
+				</div>
+				<div>
+					<h3>Featured Films:</h3>
+					<ul>
+						<li>A New Hope</li>
+						<li>The Empire Strikes Back</li>
+					</ul>
+				</div>
+				<div>
+					<h3>Starships Driven:</h3>
+					<ul>
+						<li>CR90 Corvette</li>
+						<li>Star Destroyer</li>
+						<li>Death Star</li>
+					</ul>
+				</div>
+			</ul>
+		</div>
+	);
+}
