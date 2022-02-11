e7e70dfe5982cb35d7ae45fc25229b42376576e4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-11 16:00:33 -0500

    feat(characters list page): create an array of characters to mock the dynamic rendering of fetched data

--- pages/character/index.js
+++ pages/character/index.js
@@ -1,23 +1,34 @@
 import Link from 'next/link';
 
 export default function CharactersListPage() {
+	const characters = [
+		{
+			id: 1,
+			name: 'Luke Skywalker',
+		},
+		{
+			id: 2,
+			name: 'C-3PO',
+		},
+		{
+			id: 3,
+			name: 'R2-D2',
+		},
+		{
+			id: 4,
+			name: 'Darth Vader',
+		},
+	];
 	return (
 		<div>
 			<h1>Star Wars Characters</h1>
 			<div>
 				<ul>
-					<li>
-						<Link href='/character/1'>Luke Skywalker</Link>
-					</li>
-					<li>
-						<Link href='/character/2'>C-3PO</Link>
-					</li>
-					<li>
-						<Link href='/character/3'>R2-D2</Link>
-					</li>
-					<li>
-						<Link href='/character/4'>Darth Vader</Link>
-					</li>
+					{characters.map((character) => (
+						<li key={character.id}>
+							<Link href={`/character/${character.id}`}>{character.name}</Link>
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
