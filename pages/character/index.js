9d99fb27a91c215ead08efb49bcc45c9a4353863
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-11 15:39:04 -0500

    feat(character page): create a list of character name links

--- pages/character/index.js
+++ pages/character/index.js
@@ -1,37 +1,25 @@
+import Link from 'next/link';
+
 export default function CharactersListPage() {
 	return (
 		<div>
 			<h1>Star Wars Characters</h1>
-			<ul>
-				<div>
-					<h3>About Me:</h3>
-					<ul>
-						<li>Height: 5ft 10in</li>
-						<li>Mass: 160 lbs</li>
-						<li>Gender: Male</li>
-						<li>Eye Color: Brown</li>
-						<li>Hair Color: Brown</li>
-						<li>Birth Year: 1954</li>
-						<li>Species: Human</li>
-						<li>Homeworld: Earth</li>
-					</ul>
-				</div>
-				<div>
-					<h3>Featured Films:</h3>
-					<ul>
-						<li>A New Hope</li>
-						<li>The Empire Strikes Back</li>
-					</ul>
-				</div>
-				<div>
-					<h3>Starships Driven:</h3>
-					<ul>
-						<li>CR90 Corvette</li>
-						<li>Star Destroyer</li>
-						<li>Death Star</li>
-					</ul>
-				</div>
-			</ul>
+			<div>
+				<ul>
+					<li>
+						<Link href='/character/1'>Luke Skywalker</Link>
+					</li>
+					<li>
+						<Link href='/character/2'>C-3PO</Link>
+					</li>
+					<li>
+						<Link href='/character/3'>R2-D2</Link>
+					</li>
+					<li>
+						<Link href='/character/4'>Darth Vader</Link>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
