3553cbb93bf91fd3f4fdd107cc9f7b1b63e74872
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-11 14:31:06 -0500

    feat(character page): create a dynamic route, within the 'character' directory, for 'characterid' queries

---
+++ pages/character/[characterid].js
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router';
+
+export default function CharacterPage() {
+	const router = useRouter();
+
+	console.log(router.pathname);
+	console.log(router.query);
+
+	return (
+		<div>
+			<h1>Han Solo</h1>
+			<ul>
+				<div>
+					<h3>About Me:</h3>
+					<ul>
+						<li>Height: 6ft 1in</li>
+						<li>Mass: 190 lbs</li>
+						<li>Gender: Male</li>
+						<li>Eye Color: Brown</li>
+						<li>Hair Color: Brown</li>
+						<li>Birth Year: 1951</li>
+						<li>Species: Human</li>
+						<li>Homeworld: Earth</li>
+					</ul>
+				</div>
+				<div>
+					<h3>Featured Films:</h3>
+					<ul>
+						<li>A New Hope</li>
+						<li>The Empire Strikes Back</li>
+					</ul>
+				</div>
+				<div>
+					<h3>Starships Driven:</h3>
+					<ul>
+						<li>CR90 Corvette</li>
+						<li>Star Destroyer</li>
+						<li>Death Star</li>
+					</ul>
+				</div>
+			</ul>
+		</div>
+	);
+}
