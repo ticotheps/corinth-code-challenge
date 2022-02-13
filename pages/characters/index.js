33472fcb9d240cf1280b2fc30339689147c7d1d6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-12 23:24:03 -0500

    feat(characters page): create cards on the 'CharactersPage' that display a character's name and image

--- pages/characters/index.js
+++ pages/characters/index.js
@@ -1,17 +1,19 @@
 import Link from 'next/link';
+import { getAllCharacters } from '../../dummy-data.js';
 import styles from '../../styles/CharactersPage.module.css';
 
 export default function CharactersPage() {
-	const characters = [];
+	const characters = getAllCharacters();
 
 	return (
 		<main className={styles.main}>
- 			<h1 className={styles.title}>Star Wars Characters</h1>
+			<h1 className={styles.title}>Star Wars Characters</h1>
 
 			<ul className={styles.cardContainer}>
 				{characters.map((character) => (
 					<Link href={`/characters/${character.id}`} key={character.id}>
 						<div className={styles.card}>
+							<img src={character.image} alt={character.name} />
 							<p>{character.name}</p>
 						</div>
 					</Link>
