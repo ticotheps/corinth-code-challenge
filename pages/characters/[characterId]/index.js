788080f9aabcda8cc58fcb0f1b4878668ec42558
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-13 02:25:37 -0500

    feat(<CharacterDetailsPage />): display character data on each Character Details page

--- pages/characters/[characterId]/index.js
+++ pages/characters/[characterId]/index.js
@@ -1,15 +1,41 @@
 import { useRouter } from 'next/router';
+import { getCharacterById } from '../../../dummy-data';
 import styles from '../../../styles/CharacterDetailsPage.module.css';
 
 export default function CharacterDetailsPage() {
 	const router = useRouter();
-
-	console.log(router.query);
-	console.log(router.pathname);
+	const characterId = parseInt(router.query.characterId);
+	const character = getCharacterById(characterId);
+	console.log({ character });
 
 	return (
 		<main className={styles.main}>
-			<h1 className={styles.title}>Character Details Page</h1>
+			<div className={styles.container}>
+				<h1 className={styles.title}>{character.name}</h1>
+				<div className={styles.containerRow}>
+					<img src={`../../../${character.image}`} />
+					<ul>
+						<li>
+							<b>Height:</b> {character.height} cm
+						</li>
+						<li>
+							<b>Mass:</b> {character.mass} kg
+						</li>
+						<li>
+							<b>Hair Color:</b> {character.hair_color}
+						</li>
+						<li>
+							<b>Skin Color:</b> {character.skin_color}
+						</li>
+						<li>
+							<b>Eye Color:</b> {character.eye_color}
+						</li>
+						<li>
+							<b>Birth Year:</b> {character.birth_year}
+						</li>
+					</ul>
+				</div>
+			</div>
 		</main>
 	);
 }
