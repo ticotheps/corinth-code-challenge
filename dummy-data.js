4fddff87e20ac6994fefb568518081024336b5a7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-13 00:23:05 -0500

    feat(dummy-data): create a function that can return a specific dummy character by its ID

--- dummy-data.js
+++ dummy-data.js
@@ -86,6 +86,12 @@ const DUMMY_CHARACTERS = [
 	},
 ];
 
+// Returns all characters from "DUMMY_CHARACTERS" array.
 export function getAllCharacters() {
 	return DUMMY_CHARACTERS;
+}
+
+// Returns a specific character, by a given "id", from "DUMMY_CHARACTERS" array.
+export function getCharacterById(id) {
+	return DUMMY_CHARACTERS.find((character) => character.id === id);
 }
