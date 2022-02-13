7949e2945313de2007a7aacf9f9695eb55b64c3e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-12 23:38:58 -0500

    feat(filtered characters page): create a dynamic, catch-all route for filtered characters

---
+++ pages/characters/[...slug].js
@@ -0,0 +1,9 @@
+import styles from '../../styles/FilteredCharactersPage.module.css';
+
+export default function FilteredCharactersPage() {
+	return (
+		<main className={styles.main}>
+			<h1>Filtered Characters Page</h1>
+		</main>
+	);
+}
