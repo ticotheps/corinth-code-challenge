9d952bbd476ba01774219295c22387e05690bedf
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-12 22:28:16 -0500

    refactor(character page): rename 'CharacterPage' component to 'CharacterDetailsPage' to be more semantic

--- pages/characters/[characterId]/index.js
+++ pages/characters/[characterId]/index.js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 
-export default function CharacterPage() {
+export default function CharacterDetailsPage() {
 	const router = useRouter();
 
 	console.log(router.query);
