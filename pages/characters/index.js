c6107d393fba0acce3154673b77b2f0f12362cfa
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-14 02:17:23 -0500

    style(characters page): replace custom CSS modules with styling from NextUI

--- pages/characters/index.js
+++ pages/characters/index.js
@@ -1,14 +1,29 @@
 import { getAllCharacters } from '../../dummy-data.js';
+import { Container, Grid, Row, Text, Spacer } from '@nextui-org/react';
 import CharactersList from '../../components/CharactersList';
-import styles from '../../styles/CharactersPage.module.css';
 
 export default function CharactersPage() {
 	const characters = getAllCharacters();
 
 	return (
-		<main className={styles.main}>
-			<h1 className={styles.title}>Star Wars Characters</h1>
-			<CharactersList characters={characters} />
-		</main>
+		<Container fluid>
+			<Grid.Container gap={2} justify='center'>
+				<Spacer y={3} />
+				<Row justify='center' align='center'>
+					<Text
+						h1
+						size='4em'
+						css={{
+							textGradient: '45deg, $blue500 -20%, $pink500 50%',
+						}}
+						weight='bold'
+					>
+						Star Wars Characters
+					</Text>
+				</Row>
+				<Spacer y={4} />
+				<CharactersList characters={characters} />
+			</Grid.Container>
+		</Container>
 	);
 }
