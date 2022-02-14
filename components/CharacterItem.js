71fb15f0914c1612cdd5bc019b7cf3ac030a9bbb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-14 04:18:05 -0500

    feat(<CharacterItem />): use regex to extract character ID from the 'url' property of the 'Character' object

--- components/CharacterItem.js
+++ components/CharacterItem.js
@@ -1,43 +1,51 @@
 import Link from 'next/link';
-import { Card, Row, Col, Text, Button, Spacer } from '@nextui-org/react';
+import { Grid, Card, Row, Col, Text, Button, Spacer } from '@nextui-org/react';
 
 export default function CharacterItem(props) {
-	const { id, image, name } = props.character;
-	const characterDetailsPageLink = `/characters/${id}`;
+	const { image, name, url } = props.character;
+	let numberPattern = /\d+/g;
+	const matchedNumber = url.match(numberPattern);
+	// console.log({ matchedNumber });
+	const character_id = parseInt(matchedNumber[0]);
+	console.log({ character_id });
+	const characterDetailsPageLink = `/characters/${character_id}`;
+
 	const characterCard = () => (
-		<Card hoverable>
-			<Card.Body css={{ p: 0 }}>
-				<Card.Image
-					objectFit='cover'
-					src={image}
-					width='100%'
-					height={350}
-					alt={name}
-				/>
-			</Card.Body>
-			<Col>
-				<Spacer y={1} />
-				<Row justify='center' align='center'>
-					<Text h3>{name}</Text>
-				</Row>
-				<Spacer y={1} />
-				<Row justify='center' align='center'>
-					<Button color='gradient' rounded auto ghost>
-						<Link href={characterDetailsPageLink}>
-							<Text
-								css={{ color: 'inherit' }}
-								size={12}
-								weight='bold'
-								transform='uppercase'
-							>
-								Learn More
-							</Text>
-						</Link>
-					</Button>
-				</Row>
-				<Spacer y={1} />
-			</Col>
-		</Card>
+		<Grid xs={4}>
+			<Card hoverable css={{ minWidth: '400px' }}>
+				<Card.Body>
+					<Card.Image
+						objectFit='cover'
+						src={image}
+						width='100%'
+						height={350}
+						alt={name}
+					/>
+				</Card.Body>
+				<Col>
+					<Spacer y={1} />
+					<Row justify='center' align='center'>
+						<Text h3>{name}</Text>
+					</Row>
+					<Spacer y={1} />
+					<Row justify='center' align='center'>
+						<Button color='gradient' rounded auto ghost>
+							<Link href={characterDetailsPageLink}>
+								<Text
+									css={{ color: 'inherit' }}
+									size={12}
+									weight='bold'
+									transform='uppercase'
+								>
+									Learn More
+								</Text>
+							</Link>
+						</Button>
+					</Row>
+					<Spacer y={1} />
+				</Col>
+			</Card>
+		</Grid>
 	);
 
 	return <div>{characterCard()}</div>;
