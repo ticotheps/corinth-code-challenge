0d5a51709fb87646ef9629fecdf61b6e08f1b28a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-14 01:05:47 -0500

    feat(MyApp): create a reusable <PageHead /> component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import PageHead from '../components/PageHead';
 import { NextUIProvider } from '@nextui-org/react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<NextUIProvider>
+			<PageHead />
 			<Component {...pageProps} />
 		</NextUIProvider>
 	);
