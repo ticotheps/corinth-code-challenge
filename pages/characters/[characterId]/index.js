7b735ac42ae50936183be08bd38256f8a09f5b9c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-14 02:16:54 -0500

    style(<CharacterDetailsPage />): replace custom CSS modules with styling from NextUI

--- pages/characters/[characterId]/index.js
+++ pages/characters/[characterId]/index.js
@@ -1,6 +1,14 @@
 import { useRouter } from 'next/router';
+import {
+	Container,
+	Row,
+	Col,
+	Card,
+	Text,
+	Button,
+	Spacer,
+} from '@nextui-org/react';
 import { getCharacterById } from '../../../dummy-data';
-import styles from '../../../styles/CharacterDetailsPage.module.css';
 
 export default function CharacterDetailsPage() {
 	const router = useRouter();
@@ -9,40 +17,153 @@ export default function CharacterDetailsPage() {
 
 	if (!character) {
 		return (
-			<h3 className={styles.description}>
+			<Text h1>
 				No character with that ID was found. Please go back and try again.
-			</h3>
+			</Text>
 		);
 	}
 
 	return (
-		<main className={styles.main}>
-			<div className={styles.container}>
-				<h1 className={styles.title}>{character.name}</h1>
-				<div className={styles.containerRow}>
-					<img src={`../../../${character.image}`} />
-					<ul>
-						<li>
-							<b>Height:</b> {character.height} cm
-						</li>
-						<li>
-							<b>Mass:</b> {character.mass} kg
-						</li>
-						<li>
-							<b>Hair Color:</b> {character.hair_color}
-						</li>
-						<li>
-							<b>Skin Color:</b> {character.skin_color}
-						</li>
-						<li>
-							<b>Eye Color:</b> {character.eye_color}
-						</li>
-						<li>
-							<b>Birth Year:</b> {character.birth_year}
-						</li>
-					</ul>
-				</div>
-			</div>
-		</main>
+		<Container fluid>
+			<Col>
+				<Row justify='center' align='center'>
+					<Text
+						h1
+						size='5em'
+						css={{
+							textGradient: '45deg, $blue500 -20%, $pink500 50%',
+						}}
+						weight='bold'
+					>
+						{character.name}
+					</Text>
+				</Row>
+				<Spacer y={5} />
+				<Row justify='center' align='center'>
+					<Col>
+						<Card css={{ mw: '500px' }}>
+							<Card.Body>
+								<Card.Image
+									src={`../../../${character.image}`}
+									height={400}
+									width='100%'
+									alt={character.name}
+								/>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Row>
+							<Col>
+								<Text h4 size='2rem' weight='bold'>
+									Height:
+								</Text>
+							</Col>
+							<Col>
+								<Text
+									h4
+									size='2rem'
+									css={{
+										textGradient: '45deg, $blue500 -20%, $pink500 50%',
+									}}
+								>
+									{character.height} cm
+								</Text>
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<Text h4 size='2rem' weight='bold'>
+									Mass:
+								</Text>
+							</Col>
+							<Col>
+								<Text
+									h4
+									size='2rem'
+									css={{
+										textGradient: '45deg, $blue500 -20%, $pink500 50%',
+									}}
+								>
+									{character.mass} kg
+								</Text>
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<Text h4 size='2rem' weight='bold'>
+									Hair Color:
+								</Text>
+							</Col>
+							<Col>
+								<Text
+									h4
+									size='2rem'
+									css={{
+										textGradient: '45deg, $blue500 -20%, $pink500 50%',
+									}}
+								>
+									{character.hair_color}
+								</Text>
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<Text h4 size='2rem' weight='bold'>
+									Skin Color:
+								</Text>
+							</Col>
+							<Col>
+								<Text
+									h4
+									size='2rem'
+									css={{
+										textGradient: '45deg, $blue500 -20%, $pink500 50%',
+									}}
+								>
+									{character.skin_color}
+								</Text>
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<Text h4 size='2rem' weight='bold'>
+									Eye Color:
+								</Text>
+							</Col>
+							<Col>
+								<Text
+									h4
+									size='2rem'
+									css={{
+										textGradient: '45deg, $blue500 -20%, $pink500 50%',
+									}}
+								>
+									{character.eye_color}
+								</Text>
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<Text h4 size='2rem' weight='bold'>
+									Birth Year:
+								</Text>
+							</Col>
+							<Col>
+								<Text
+									h4
+									size='2rem'
+									css={{
+										textGradient: '45deg, $blue500 -20%, $pink500 50%',
+									}}
+								>
+									{character.birth_year}
+								</Text>
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+			</Col>
+		</Container>
 	);
 }
