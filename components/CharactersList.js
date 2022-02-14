d1a4c91a0632682104dd486b97c67907983576ea
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-14 04:07:50 -0500

    feat(<CharactersList />): retrieve live data from the SWAPI.dev API

--- components/CharactersList.js
+++ components/CharactersList.js
@@ -1,16 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import CharacterItem from './CharacterItem';
-import { Grid } from '@nextui-org/react';
+import { Grid, Loading, Text, Row } from '@nextui-org/react';
 
 export default function CharactersList(props) {
-	const { characters } = props;
+	const [data, setData] = useState(null);
+	const [isLoading, setLoading] = useState(false);
 
+	useEffect(() => {
+		setLoading(true);
+		fetch('https://swapi.dev/api/people/')
+			.then((res) => res.json())
+			.then((data) => {
+				setData(data);
+				setLoading(false);
+			});
+	}, []);
+
+	if (isLoading) return <Loading size='xl' type='points' />;
+	if (!data) return <Text h2>Something went wrong. Please try again.</Text>;
+	console.log({ data });
 	return (
-		<Grid.Container gap={2} justify='center'>
-			{characters.map((character) => (
-				<Grid xs key={character.id}>
-					<CharacterItem character={character} key={character.id} />
-				</Grid>
+		<>
+			{data.results.map((character, index) => (
+				<CharacterItem character={character} key={index + 1} />
 			))}
-		</Grid.Container>
+		</>
 	);
 }
