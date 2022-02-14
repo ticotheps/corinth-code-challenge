48ee6016be5d2955d360f899ab12bee3ab07f3d9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-14 04:20:59 -0500

    fix(characters page): remove extra <Grid.Container> element from 'CharactersPages'

--- pages/characters/index.js
+++ pages/characters/index.js
@@ -7,21 +7,21 @@ export default function CharactersPage() {
 
 	return (
 		<Container fluid>
+			<Spacer y={3} />
+			<Row justify='center' align='center'>
+				<Text
+					h1
+					size='4em'
+					css={{
+						textGradient: '45deg, $blue500 -20%, $pink500 50%',
+					}}
+					weight='bold'
+				>
+					Star Wars Characters
+				</Text>
+			</Row>
+			<Spacer y={4} />
 			<Grid.Container gap={2} justify='center'>
-				<Spacer y={3} />
-				<Row justify='center' align='center'>
-					<Text
-						h1
-						size='4em'
-						css={{
-							textGradient: '45deg, $blue500 -20%, $pink500 50%',
-						}}
-						weight='bold'
-					>
-						Star Wars Characters
-					</Text>
-				</Row>
-				<Spacer y={4} />
 				<CharactersList characters={characters} />
 			</Grid.Container>
 		</Container>
