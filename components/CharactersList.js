9b1315b9daade1ac46dc5cdb503b82d3a64f1727
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-14 22:52:15 -0500

    fix(<CharactersList />): fix conditionally-rendered content based on fetched data

--- components/CharactersList.js
+++ components/CharactersList.js
@@ -1,28 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import CharacterItem from './CharacterItem';
-import { Grid, Loading, Text, Row } from '@nextui-org/react';
+import CharacterProfile from './CharacterProfile';
+import { Loading, Text, Row, Grid } from '@nextui-org/react';
 
-export default function CharactersList(props) {
+export default function CharactersList() {
 	const [data, setData] = useState(null);
 	const [isLoading, setLoading] = useState(false);
 
 	useEffect(() => {
 		setLoading(true);
 		fetch('https://swapi.dev/api/people/')
-			.then((res) => res.json())
+			.then((res) => {
+				let responseHeader = res.headers.get('next');
+				res.json();
+			})
 			.then((data) => {
 				setData(data);
 				setLoading(false);
 			});
 	}, []);
 
-	if (isLoading) return <Loading size='xl' type='points' />;
-	if (!data) return <Text h2>Something went wrong. Please try again.</Text>;
-	console.log({ data });
+	if (isLoading) {
+		return (
+			<Row justify='center' align='center'>
+				<Loading size='xl' type='points' />
+			</Row>
+		);
+	}
+
+	if (!data) {
+		return (
+			<>
+				<Row justify='center' align='center'>
+					<Text h2>Something went wrong.</Text>
+				</Row>
+				<Row justify='center' align='center'>
+					<Text h2>Please refresh the page and try again.</Text>
+				</Row>
+			</>
+		);
+	}
+
+	console.log('data (<CharactersList />) = ', data);
 	return (
 		<>
 			{data.results.map((character, index) => (
-				<CharacterItem character={character} key={index + 1} />
+				<CharacterItem
+					character={character}
+					characterId={index + 1}
+					key={index}
+				/>
 			))}
 		</>
 	);
