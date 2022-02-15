128b33c2ec9af72f282294add8897dad8ed0cea8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-15 14:20:38 -0500

    style: fix mobile-responsiveness

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,19 @@
 import Image from 'next/image';
-import { Col, Text, Spacer, Link } from '@nextui-org/react';
+import { Col, Row, Text, Spacer, Link } from '@nextui-org/react';
 
 export default function Footer() {
 	return (
 		<Col align='center'>
-			<Spacer y={7} />
+			<Spacer y={6} />
 			<Text>
-				Powered by{' '}
-				<Link href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'>
-					<Image src='/vercel.svg' alt='Vercel Logo' width={60} height={12} />
-				</Link>
+				<Row align='center' justify='center'>
+					<Text>Powered by</Text>
+					<Link href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'>
+						<Image src='/vercel.svg' alt='Vercel Logo' width={85} height={16} />
+					</Link>
+				</Row>
+				<Spacer y={2} />
 			</Text>
-			<Spacer y={1} />
 		</Col>
 	);
 }
