a59c8c91fa386b38d478aa50c61d9353bbdc300e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-18 14:16:59 -0500

    chore(<MyApp />): configure react-query and react-query-devtools

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import Layout from '../components/Layout';
 import { NextUIProvider } from '@nextui-org/react';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
+	const [queryClient] = React.useState(() => new QueryClient());
 	return (
-		<NextUIProvider>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</NextUIProvider>
+		<QueryClientProvider client={queryClient}>
+			<NextUIProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</NextUIProvider>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
