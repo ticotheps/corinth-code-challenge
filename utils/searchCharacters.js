95fdd29bc6344a932a51974deabc685453c186ce
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-18 23:32:00 -0500

    feat(utils): create a new utils function that allows a user to search our JSON file for a character by name

---
+++ utils/searchCharacters.js
@@ -0,0 +1,30 @@
+import mock_data from '../data/mock_data.json';
+
+export default function searchCharacters(query) {
+	return new Promise((resolve) => {
+		// helper function that searches for characters' names that contain
+		// characters from the given query.
+		function queryInCharName(charName, query) {
+			return charName.toLowerCase().includes(query.toLowerCase());
+		}
+
+		// returns only those character objects whose names contain the
+		// characters of the given query.
+		const matchingCharObjects = mock_data.people.filter((character) => {
+			return queryInCharName(character.name, query);
+		});
+
+		// returns a new array of objects that contain the 'name' and the 'url'
+		// properties of the matching characters.
+		const matchingCharNames = matchingCharObjects.map((character) => {
+			return {
+				name: character.name,
+				url: character.url,
+			};
+		});
+
+		setTimeout(() => {
+			resolve(matchingCharNames);
+		}, 500);
+	});
+}
