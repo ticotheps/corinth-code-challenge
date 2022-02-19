d157429a3efae4c529c54521c3a28f4ae6eb5579
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-19 01:28:54 -0500

    feat(<SearchResults />): create a new component that will list individual character cards based on the text input from the user in the search input box

---
+++ components/SearchResults.js
@@ -0,0 +1,34 @@
+import { Grid, Col, Link, Card, Row, Text } from '@nextui-org/react';
+
+export default function SearchResults(props) {
+	const { data } = props;
+	// console.log({ data });
+
+	const results = data.map((character, index) => {
+		const characterUrl = character.url;
+		const characterId = characterUrl.split('/').slice(-2)[0];
+
+		return (
+			<Grid key={index}>
+				<Col justify='center'>
+					<Link href={`/characters/${characterId}`}>
+						<Card hoverable clickable bordered css={{ minWidth: '180px' }}>
+							<Row justify='center' align='center'>
+								<Text
+									size='1.1rem'
+									css={{
+										textGradient: '45deg, blue -10%, purple 80%',
+									}}
+								>
+									{character.name}
+								</Text>
+							</Row>
+						</Card>
+					</Link>
+				</Col>
+			</Grid>
+		);
+	});
+
+	return results;
+}
