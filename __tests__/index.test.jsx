9faea93d65cd56dddfdbd4f50dd97a0c84d8b3ca
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-28 00:54:04 -0500

    test: add another test suite

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,9 +2,15 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import HomePage from '../pages/index';
 
-test('renders three header elements on Home Page', () => {
+test('renders three header elements on the Home Page', () => {
 	render(<HomePage />);
 	expect(screen.getByText(/find them/i)).toBeInTheDocument();
 	expect(screen.getByText(/meet them/i)).toBeInTheDocument();
 	expect(screen.getByText(/greet them/i)).toBeInTheDocument();
+});
+
+test('renders two button elements on the Home Page', () => {
+	render(<HomePage />);
+	expect(screen.getByText('Browse All Characters')).toBeInTheDocument();
+	expect(screen.getByText('Search Character Name')).toBeInTheDocument();
 });
