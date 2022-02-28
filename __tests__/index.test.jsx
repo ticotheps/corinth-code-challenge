10019cdb8cefe2a861cf6a5c876685f742bd5013
Tico S. Thepsourinthone <ticotheps@gmail.com>
2022-02-28 01:31:38 -0500

    chore: don't forget to include imports

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import HomePage from '../pages/index';
+import CharactersPage from '../pages/characters/index';
+import SearchPage from '../pages/characters/search';
 
 test('renders three header elements on the Home Page', () => {
 	render(<HomePage />);
@@ -13,4 +15,9 @@ test('renders two button elements on the Home Page', () => {
 	render(<HomePage />);
 	expect(screen.getByText(/browse all characters/i)).toBeInTheDocument();
 	expect(screen.getByText(/search character name/i)).toBeInTheDocument();
+});
+
+test('renders a single "Browse All Characters" button underneath the input box on the Search Page', () => {
+	render(<SearchPage />);
+	expect(screen.getByText(/browse all characters/i)).toBeInTheDocument();
 });
